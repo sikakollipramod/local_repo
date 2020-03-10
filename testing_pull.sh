4ab030c910eeae8f8a4422134e7990a2d3a41a5c
sikakollipramod <60956615+sikakollipramod@users.noreply.github.com>
2020-03-10 16:36:39 +0530

    Update testing_pull.sh
    testing webhooks

--- testing_pull.sh
+++ testing_pull.sh
@@ -3,3 +3,5 @@ hi this is new file to test the pull from hub to local server.
 added new line to test the jenkins job
 
 added new line second time on 10th mar to test jenkins CI-JOB job
+
+above didnt trigger due to webhooks change in the ip address. now i cahnged it ..it should trigger now. with thisline .
